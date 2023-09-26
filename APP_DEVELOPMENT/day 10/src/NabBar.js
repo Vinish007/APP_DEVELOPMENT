0de121829ddd2321445f6607b44e0e4c9f4d29d4
Vinish007 <110531751+Vinish007@users.noreply.github.com>
2023-09-26 14:13:31 +0530

    Add files via upload

---
+++ APP_DEVELOPMENT/day 10/src/NabBar.js
@@ -0,0 +1,53 @@
+import React, { useContext} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { RiSearchLine } from 'react-icons/ri';
+import TemporaryDrawer from './Sidebar';
+import { SearchContext } from './searchContext';
+import '../src/NavBar.css'
+function Navbar() {
+    const { searchQuery, handleSearchQueryChange } = useContext(SearchContext);
+
+  const handleSearchInputChange = (e) => {
+    handleSearchQueryChange(e.target.value);
+  };
+    return (
+        <>
+            <div className="header">
+                <div className="space">
+                    <div className='log'>
+                        <TemporaryDrawer />
+                        <Link to="/" class="logo">WildV</Link>
+                    </div>
+                    <div className='search'>
+                    <form >
+                        <input type="text" placeholder="Search..." className="searchInput" value={searchQuery} onChange={handleSearchInputChange}/>
+                        <button className="searchButton">
+                            <RiSearchLine className="search-icon" />
+                        </button>
+                    </form>
+                    </div>
+                    <div class="header-right">
+                        <Link to="/" class="active" ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+                        </svg></Link>
+                        <Link to="/signin"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                            <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                        </svg></Link>
+                        <Link to="/profile">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
+                            </svg>
+                        </Link>
+                       {/* <Link to="/cart"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+    </svg></Link>*/}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
