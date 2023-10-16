c0aa1d347a05493e684cfd54aaef8cacdb537a4b
Vinish007 <110531751+Vinish007@users.noreply.github.com>
2023-10-16 16:09:39 +0530

    Add files via upload

---
+++ APP_DEVELOPMENT/day23/Backend/api_gateway-1/src/main/java/com/example/demo/ApiGateway1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class ApiGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway1Application.class, args);
+	}
+	
+}
